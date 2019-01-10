8e5a282319fd8fe0ac17b5b484cc984a51bc1808
hharris0803 <hchang0803@outlook.com>
2019-01-09 21:59:52 -0500

    the secrets out - helen is learning react-native

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+import Main from './src/components/Main';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
---
+++ src/components/Main.js
@@ -0,0 +1,124 @@
+
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    ScrollView,
+    TouchableOpacity
+} from 'react-native';
+
+import Note from './Note';
+
+export default class Main extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            noteArray: [],
+            noteText: '',
+        };
+    }
+    render() {
+        let notes = this.state.noteArray.map((val, key)=>{
+            return <Note key={key} keyval={key} val={val}
+                    deleteMethod={()=>this.deleteNote(key)}/>
+        });
+        return (
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.headerText}> To Do List </Text>
+                </View>
+                <ScrollView style={styles.scrollContainer}>
+                    {notes}
+                </ScrollView>
+
+                <View style={styles.footer}>
+                    <TextInput 
+                        style={styles.textInput}
+                        placeholder='>Add New Note'
+                        onChangeText={(noteText)=> this.setState({noteText})}
+                        value={this.state.noteText}
+                        placeholderTextColor='#fff'
+                        >
+                    </TextInput>
+                </View>
+                <TouchableOpacity onPress={ this.addNote.bind(this) } style={styles.addButton}>
+                    <Text style={styles.addButtonText}>+</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
+    addNote(){
+        if(this.state.noteText){
+            let d = new Date();
+            this.state.noteArray.push({
+                'note': this.state.noteText
+            });
+            this.setState({ noteArray: this.state.noteArray });
+            this.setState({noteText:''});
+        }
+    }
+
+    deleteNote(key){
+        this.state.noteArray.splice(key, 1);
+        this.setState({noteArray: this.state.noteArray});
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: '#ffc0cb',
+        alignItems: 'center',
+        justifyContent:'center',
+        borderBottomWidth: 20,
+        borderBottomColor: '#ffffff'
+    },
+    headerText: {
+        color: '#000000',
+        fontSize: 25,
+        padding: 30
+    },
+    scrollContainer: {
+        flex: 1,
+        marginBottom: 100
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 10
+    },
+    textInput: {
+        alignSelf: 'stretch',
+        color: '#fff',
+        padding: 20,
+        backgroundColor: '#252525',
+        borderTopWidth:2,
+        borderTopColor: '#ffffff'
+    },
+    addButton: {
+        position: 'absolute',
+        zIndex: 11,
+        right: 20,
+        bottom: 90,
+        backgroundColor: '#ffc0cb',
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 8
+    },
+    addButtonText: {
+        color: '#000000',
+        fontSize: 24
+    }
+});
